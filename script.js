095c1089e658a6094c3b247c0ad21f58d1010404
RiicharMc <116658077+RiicharMc@users.noreply.github.com>
2024-09-03 19:22:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,14 +6,14 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
 {
-    enunciado: "Quais são os desafios enfrentados na criação de tecnologias acessíveis e como podem ser superados?",
+    enunciado: "O Rafael é gay?",
     alternativas: [
         {
-            texto:"Custos elevados",
+            texto:"sim",
             afirmacao: "afirmação 1"
         },
         {
-        texto:"Conscientização e educação",
+        texto:"sem duvidas",
         afirmacao: "afirmação 2"
         }
     ]
